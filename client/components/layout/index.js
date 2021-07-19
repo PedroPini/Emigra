19852ae151f40fabf24696e212ff69849c3113e7
Abid Ali <abdsoftfsd@gmail.com>
2021-07-19 14:58:46 +0500

    fixes unmatched tag error

--- client/components/layout/index.js
+++ client/components/layout/index.js
@@ -1,44 +1,49 @@
-import React from 'react'
-import cn from 'classnames'
+import React, { useEffect, useState } from 'react';
+import cn from 'classnames';
 
-import CONST from '../../constants'
-import useWindowSize from '../../hooks/useWindowSize'
+import CONST from '../../constants';
+import useWindowSize from '../../hooks/useWindowSize';
 
-import Sidebar from './sidebar'
-import Main from './main'
-import Extra from './extra'
-import Header from './header'
+import Sidebar from './sidebar';
+import Main from './main';
+import Extra from './extra';
+import Header from './header';
 
-import styles from './layout.module.css'
-import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import styles from './layout.module.css';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
   colors: {
     brand: {
-      100: "#d53f8c",
-     
+      100: '#d53f8c',
+
     },
     blue: {
-      1000: 'rgb(7, 48, 93);'
-    }
+      1000: 'rgb(7, 48, 93);',
+    },
   },
-})
+});
 
 const Layout = ({ extra = true, children }) => {
-  const size = useWindowSize()
+  let [size, setSize] = useState({});
+  const windowSize = useWindowSize();
+  useEffect(() => {
+    setSize(windowSize);
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
-      <Header />
-      
+      <Header/>
+
       <div className={styles.container}>
         <div className={cn(styles.body, !extra && styles.main)}>
-          {size.width > CONST.MOBILE_SIZE && <Sidebar />}
+          {size.width > CONST.MOBILE_SIZE && <Sidebar/>}
           <Main>{children}</Main>
-          {size.width > CONST.TABLET_SIZE && extra && <Extra />}
+          {size.width > CONST.TABLET_SIZE && extra && <Extra/>}
         </div>
       </div>
-      </ChakraProvider>
-  )
-}
+    </ChakraProvider>
+  );
+};
 
-export default Layout
+export default Layout;
